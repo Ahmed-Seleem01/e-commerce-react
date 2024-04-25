88edfa583cdbe0e620339b937aa2138cd4c16fe7
Ahmed <ahmedseleem.c@gmail.com>
2024-04-25 20:31:15 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'Poppins': ['Poppins', 'sans-serif'],
+      'Inter': ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
