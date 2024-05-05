f0b6a60caf1217076141f4cd43c19049e83da4d1
Ahmed <ahmedseleem.c@gmail.com>
2024-05-05 12:23:25 +0300

    Modify the error to display current error using router error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export const Error = () => {
+  const error = useRouteError();
+  console.log({ error });
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex min-h-[100vh] flex-col items-center justify-center">
       <h2 className=" font-Inter text-[110px]/[115px] font-medium">
         404 Not Found
       </h2>
       <p className="mb-20 mt-10">
-        Your visited page not found. You may go home page.
+        {error.status === 404
+          ? "Your visited page not found. You may go home page."
+          : error.statusText || error.message}
       </p>
 
       <Link to="">
