f27ad8eb91a99f81b337eb2dab7e7d45c9ca1317
Ahmed <ahmedseleem.c@gmail.com>
2024-05-05 11:38:44 +0300

    Add the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,44 @@
-import { Footer } from "./Footer";
-import { Header } from "./Header";
-import { SignUP } from "./SignUp";
+import { Ad } from "./Ad";
+import { BestSelling } from "./BestSelling";
+import { Category } from "./Category";
+import { ExploreProducts } from "./ExploreProducts";
+import { FlashSales } from "./FlashSales";
+import { NewArrival } from "./NewArrival";
+import { OpenningSection } from "./OpenningSection";
+
+// import { SignIn } from "./SignIn";
+// import { SignUP } from "./SignUp";
+// const products = [
+//   {
+//     image: lcd,
+//     heading: "LCD Monitor",
+//     price: "650",
+//   },
+//   {
+//     image: gamepad,
+//     heading: "H1Gamepad",
+//     price: "550",
+//   },
+// ];
 
 export const Home = () => {
   return (
-    <div className="mx-auto my-0 grid min-h-[100vh] max-w-[1170px] grid-cols-1 grid-rows-[144px_1fr_450px] justify-items-center font-Poppins font-normal">
-      <Header />
-      <SignUP />
-      <Footer />
+    <div className="flex w-[100%] flex-col gap-[140px] ">
+      <OpenningSection />
+      <FlashSales />
+      <Category />
+      <BestSelling />
+      <Ad />
+      <ExploreProducts />
+      <NewArrival />
     </div>
+
+    // {/* <Wishlist listLength="4" /> */}
+    // {/* <Cart products={products} /> */}
+    // {/* <Checkout products={products} /> */}
+    // {/* <About /> */}
+    // {/* <Contact /> */}
+    // {/* <Error /> */}
+    // {/* <Account /> */}
   );
 };
