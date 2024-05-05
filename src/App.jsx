8e1443e85094e283eaa2841a0dddbc29297990ac
Ahmed <ahmedseleem.c@gmail.com>
2024-05-05 11:34:45 +0300

    Setup react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
-import { Home } from "./components";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { LayoutPage } from "./pages";
+
+import { load as loadCart } from "./components/Cart";
+
+import {
+  About,
+  Account,
+  Cart,
+  Contact,
+  Error,
+  Home,
+  SignUP,
+  Wishlist,
+} from "./components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutPage />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUP />,
+      },
+      {
+        path: "wishlist",
+        element: <Wishlist />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+        loader: loadCart,
+      },
+      {
+        path: "account",
+        element: <Account />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
