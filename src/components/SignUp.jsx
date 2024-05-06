f1a3ea08a7ab6e3a5332f8dc79e02b4734a24b78
Ahmed <ahmedseleem.c@gmail.com>
2024-05-06 19:41:08 +0300

    Setup the signup feature

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,60 @@
 import sideImage from "../assets/images/Side Image.png";
 import googleIcon from "../assets/icons/Icon-Google.svg";
+import {
+  signUpAndSignInWithGoogle,
+  signUpWithEmailAndPassword,
+} from "../authServices";
+import { useRef, useState } from "react";
+import { updateProfile } from "firebase/auth";
 
 export const SignUP = () => {
+  const nameRef = useRef();
+  const mailRef = useRef();
+  const passwordRef = useRef();
+
+  const [message, setMessage] = useState("");
+  // Function to redirect to the home page
+  const redirectToHomePage = () => {
+    // Redirect to the home page
+    window.location.href = "/";
+  };
+
+  const signUpWithGoogleHandler = () => {
+    signUpAndSignInWithGoogle();
+    redirectToHomePage();
+  };
+
+  const signUpWithEmailAndPasswordHandler = (e) => {
+    e.preventDefault();
+    signUpWithEmailAndPassword(
+      nameRef.current.value,
+      mailRef.current.value,
+      passwordRef.current.value,
+    )
+      .then((userCredential) => {
+        // status.success = "successful sign up";
+        const user = userCredential.user;
+        redirectToHomePage();
+        // Update the user's display name
+        updateProfile(user, {
+          displayName: nameRef.current.value,
+        })
+          .then(() => {
+            // Update successful
+            console.log("Display name set:", nameRef.current.value);
+          })
+          .catch((error) => {
+            // An error occurred
+            console.error("Update profile error:", error.message);
+          });
+      })
+      .catch((error) => {
+        // An error occurred
+        setMessage(`Sign-up error: ${error.message}`);
+        console.error("Sign-up error:", error.code, error.message);
+      });
+  };
+
   return (
     <div className="mb-[140px] mt-[60px] flex w-[100%] items-center justify-between">
       <img
@@ -9,7 +62,7 @@ export const SignUP = () => {
         src={sideImage}
         alt="side page image"
       />
-      <form className=" w-[370px]">
+      <form onSubmit={signUpWithEmailAndPasswordHandler} className=" w-[370px]">
         <div className="mb-12">
           <h2 className=" mb-6 font-Inter text-[36px]/[30px] font-semibold">
             Create an account
@@ -23,6 +76,8 @@ export const SignUP = () => {
             id="name"
             name="name"
             placeholder="Name"
+            ref={nameRef}
+            required
           />
           <input
             className="border-b-[1px] outline-none"
@@ -30,6 +85,8 @@ export const SignUP = () => {
             name="email-phone"
             id="email-phone"
             placeholder="Email or Phone Number"
+            ref={mailRef}
+            required
           />
           <input
             className="border-b-[1px] outline-none"
@@ -37,12 +94,18 @@ export const SignUP = () => {
             name="password"
             id="password"
             placeholder="Password"
+            ref={passwordRef}
+            required
           />
+          {message && <div>{message}</div>}
           <div className="mb-8 flex flex-col gap-4">
             <button className=" rounded-[4px] bg-[#DB4444] py-4 text-[16px]/6 font-medium text-[#FAFAFA]">
               Create Account
             </button>
-            <button className="flex items-center justify-center gap-4 rounded-[4px] border-[1px] py-4 text-[16px]/6 font-normal">
+            <button
+              onClick={signUpWithGoogleHandler}
+              className="flex items-center justify-center gap-4 rounded-[4px] border-[1px] py-4 text-[16px]/6 font-normal"
+            >
               <img src={googleIcon} alt="google icon" />
               Sign up with Google
             </button>
@@ -50,7 +113,9 @@ export const SignUP = () => {
         </div>
         <div className="flex items-center justify-center gap-4 text-gray-600">
           <span>Already have account?</span>
-          <a className=" font-medium underline">Log in</a>
+          <a href="/login" className=" font-medium underline">
+            Log in
+          </a>
         </div>
       </form>
     </div>
