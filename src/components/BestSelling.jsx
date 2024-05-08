a4d18fa708cf7f482983bf37a97a55f7dedf31f9
Ahmed <ahmedseleem.c@gmail.com>
2024-05-08 19:36:09 +0300

    refactore to use data from firestore

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -1,11 +1,7 @@
 import { GeneralHeader } from "./GeneralHeader";
 import { ItemCard } from "./ItemCard";
-import jacket from "../assets/images/items/jacket.png";
-import bag from "../assets/images/items/bag.png";
-import speaker from "../assets/images/items/speaker.png";
-import table from "../assets/images/items/table.png";
 
-export const BestSelling = () => {
+export const BestSelling = ({ cards }) => {
   return (
     <div className="flex w-[100%] flex-col gap-[140px] ">
       <div className="flex w-[105%] flex-col gap-10 pr-10">
@@ -15,33 +11,16 @@ export const BestSelling = () => {
           viewButton
         />
         <div className=" flex  gap-[30px]">
-          <ItemCard
-            cardImage={jacket}
-            heading="The north coat"
-            currentPrice="260"
-            oldPrice="360"
-            rating="65"
-          />
-          <ItemCard
-            cardImage={bag}
-            heading="Gucci duffle bag"
-            currentPrice="960"
-            oldPrice="1160"
-            rating="65"
-          />
-          <ItemCard
-            cardImage={speaker}
-            heading="RGB liquid CPU Cooler"
-            currentPrice="160"
-            oldPrice="170"
-            rating="65"
-          />
-          <ItemCard
-            cardImage={table}
-            heading="Small BookSelf"
-            currentPrice="360"
-            rating="65"
-          />
+          {cards.map((card) => (
+            <ItemCard
+              key={card.productId}
+              cardImage={card.mainImage}
+              heading={card.heading}
+              currentPrice={card.currentPrice}
+              oldPrice={card.oldPrice}
+              rating={card.rating}
+            />
+          ))}
         </div>
       </div>
     </div>
