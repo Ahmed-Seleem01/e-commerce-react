397add4ded0a1067f02f80019ccdefb1ad455002
Ahmed <ahmedseleem.c@gmail.com>
2024-05-12 21:49:10 +0300

    Pending UI while navigating

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { Footer } from "../components";
 import { Header } from "../components";
 
 export const LayoutPage = () => {
+  const navigation = useNavigation();
   return (
-    <div className="mx-auto my-0 grid min-h-[100vh] max-w-[1170px] grid-cols-1 grid-rows-[144px_minmax(450px,_1fr)_450px] justify-items-center font-Poppins font-normal">
+    <div className=" mx-auto my-0 grid min-h-[100vh] max-w-full grid-cols-1 grid-rows-[144px_minmax(450px,_1fr)_450px] justify-items-center font-Poppins font-normal lg:max-w-[1170px]">
       <Header />
-      <div className="mb-[140px] w-full">
+      <div
+        className={`"mb-[140px] w-full ${navigation.state === "loading" ? "opacity-40 transition-opacity duration-200 ease-in-out" : ""}`}
+      >
         <Outlet />
       </div>
       <Footer />
