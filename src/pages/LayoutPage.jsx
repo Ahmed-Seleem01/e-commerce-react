90e67bea6a0a2143736e111acf31172a7b7a1959
Ahmed <ahmedseleem.c@gmail.com>
2024-05-15 23:38:42 +0300

    Fix the layout

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -16,7 +16,7 @@ export const LayoutPage = () => {
     <div className=" mx-auto my-0 grid min-h-[100vh] max-w-full grid-cols-1 grid-rows-[144px_minmax(450px,_1fr)_450px] justify-items-center font-Poppins font-normal lg:max-w-[1170px]">
       <Header q={q} />
       <div
-        className={`"mb-[140px] w-full ${navigation.state === "loading" ? "opacity-40 transition-opacity duration-200 ease-in-out" : ""}`}
+        className={`" mb-[140px] w-full ${navigation.state === "loading" ? "opacity-40 transition-opacity duration-200 ease-in-out" : ""}`}
       >
         <Outlet />
       </div>
